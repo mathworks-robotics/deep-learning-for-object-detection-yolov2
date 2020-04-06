014e402f3f09b90be38d747c3c4435467cd1cfac
cdsouza <connell.d.souza@mathworks.com>
2020-04-06 15:16:14 -0400

    Changed folder structure of project to fix some path issues

--- addFilestoPath.m
+++
@@ -1,8 +0,0 @@
-function addFilestoPath
-
-% Get the current course folder
-rootDir = fileparts(mfilename('fullpath'));
-
-% Add folders to the path
-addpath(genpath(rootDir));
-% rmpath(fullfile('Utilities','customAutomationClasses'));
